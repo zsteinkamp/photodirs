3dd68ba7161a3259d3120b7284c6c7adf6e95929
Zack Steinkamp <zack@steinkamp.us>
2023-02-20 21:25:10 -0800

    pre-generate jpegs for new raw files using chokidar

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,34 @@
 'use strict';
 
+const chokidar = require('chokidar');
 const express = require('express');
 const app = express();
 const port = 3000;
+
 const handlers = require('./handlers');
+const utils = require('./utils');
+const C = require('./constants');
+
+/*
+ * Set up album directory watcher. Currently pre-generates jpegs from raw files.
+ */
+const chokidarDebounce = {};
+chokidar.watch(C.ALBUMS_ROOT, { ignoreInitial: true }).on('all', (event, path) => {
+  console.log('WATCH', { event, path });
+  if (chokidarDebounce[path]) {
+    clearTimeout(chokidarDebounce[path]);
+  }
+  chokidarDebounce[path] = setTimeout(async (event, path) => {
+    if (event === 'add' || event === 'change') {
+      if (utils.isRaw(path)) {
+        // convert raw file
+        const cachePath = await utils.jpegFileForRaw(path);
+        console.log('PRE-CONVERT RAW', { path, cachePath });
+      }
+    }
+    chokidarDebounce[path] = null;
+  }, 1000, event, path); // last args are passed as args to callback
+});
 
 // handles api root (HATEOS)
 app.get(new RegExp('^/api/?$'), async (req, res) => {
