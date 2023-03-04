776b5797901e7aa0957a0a64d185921fb3545431
Zack Steinkamp <zack@steinkamp.us>
2023-03-04 09:24:47 -0800

    add ffmpeg; fix mobile layout

--- frontend/src/AlbumList.js
+++ frontend/src/AlbumList.js
@@ -10,7 +10,7 @@ export default function AlbumList(props) {
   let albumList = null;
   if (props.albums && props.albums.length > 0) {
     const albumListItems = props.albums.map((album) => (
-      <div key={album.apiPath}>
+      <div className="AlbumItem" key={album.apiPath}>
         <Link to={ album.uriPath }>
           <div className='thumbnail'>
             <img src={ `${album.thumbnail}?size=300x300&crop)` } loading="lazy" alt={ album.title } />
