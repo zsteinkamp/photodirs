5d3b31bfa7ad53678ff1cc99f348eca0261dbf3f
Zack Steinkamp <zack@steinkamp.us>
2023-03-06 15:33:29 -0800

    stray console

--- frontend/src/ThumbnailImg.js
+++ frontend/src/ThumbnailImg.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import VideoIcon from "./VideoIcon";
 
 const ThumbnailImg = ({data, file}) => {
-  console.log(data);
   return (
     <Link preventScrollReset={true} className={'thumbnailLink' + (file.uriPath === data.uriPath ? ' sel' : '')} to={ file.uriPath } title={ file.title }>
       <img src={ `${file.photoPath}?size=300x300&crop` } alt={ file.title }/>
