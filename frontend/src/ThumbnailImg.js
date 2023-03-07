79198d3fb7200057f65bcde104b1232ab68be634
Zack Steinkamp <zack@steinkamp.us>
2023-03-06 18:47:19 -0800

    nice thumbnail centering

--- frontend/src/ThumbnailImg.js
+++ frontend/src/ThumbnailImg.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 import VideoIcon from "./VideoIcon";
 
 const ThumbnailImg = ({data, file}) => {
+  const elemRef = useRef(null);
+
+  const scrollCenter = () => {
+    elemRef.current.scrollIntoView({ behavior: "smooth", inline: "center"});
+  };
+
+  useEffect(() => {
+    if (file.uriPath === data.uriPath) {
+      scrollCenter();
+    }
+  });
+
+
   return (
-    <Link preventScrollReset={true} className={'thumbnailLink' + (file.uriPath === data.uriPath ? ' sel' : '')} to={ file.uriPath } title={ file.title }>
+    <Link ref={elemRef} onClick={scrollCenter} preventScrollReset={true} className={'thumbnailLink' + (file.uriPath === data.uriPath ? ' sel' : '')} to={ file.uriPath } title={ file.title }>
       <img src={ `${file.photoPath}?size=300x300&crop` } alt={ file.title }/>
       {(file.type === 'video') && <VideoIcon />}
     </Link>
