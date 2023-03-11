78535c740ae3c0da1a39975ce6ef76137a034f91
Zack Steinkamp <zack@steinkamp.us>
2023-03-10 18:55:40 -0800

    dynamic index.html for opengraph stuff

---
+++ frontend/server/index.js
@@ -0,0 +1,53 @@
+const fsp = require('fs/promises');
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+// static resources should just be served as they are
+app.use(express.static(
+  path.resolve(__dirname, '..', 'build'),
+  { maxAge: '30d' }
+));
+
+const indexPath  = path.resolve(__dirname, '..', 'build', 'index.html');
+app.get('/*', async (req, res, next) => {
+  let htmlData;
+  try {
+    htmlData = await fsp.readFile(indexPath, 'utf8');
+  } catch (err) {
+    console.error('Error during file reading', err);
+    return res.status(404).end()
+  }
+
+  const url = path.join('http://api:3000/api/albums', req.path)
+  const resp = await fetch(url);
+  let body;
+  try {
+    body = await resp.json();
+  } catch (err) {
+    console.error('Error during body reading or parsing', err);
+    return res.status(404).end()
+  }
+
+  // inject meta tags
+  htmlData = htmlData.replace(
+      "<title>React App</title>",
+      `<title>${body.title}</title>`
+    )
+    .replace('__META_OG_TITLE__', body.title)
+    .replace('__META_OG_DESCRIPTION__', body.description)
+    .replace('__META_DESCRIPTION__', body.description)
+    .replace('__META_OG_IMAGE__', body.thumbnail)
+
+  console.log(htmlData);
+  return res.send(htmlData);
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    return console.log('Error during app startup', error);
+  }
+  console.log("listening on " + PORT + "...");
+});
+
