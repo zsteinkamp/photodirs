2273708fb5fd5e160854fd6befbb7c2bf5d54a25
Zack Steinkamp <zack@steinkamp.us>
2024-03-04 09:27:07 -0800

    nice and springy layouts

--- frontend/src/FileList.js
+++ frontend/src/FileList.js
@@ -1,32 +1,31 @@
-import './FileList.css';
+import './FileList.css'
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
-import VideoIcon from "./VideoIcon";
+import VideoIcon from './VideoIcon'
 
 export default function FileList(props) {
-  let fileList = null;
-
+  let fileList = null
 
   if (props.files && props.files.length > 0) {
     const fileListItems = props.files.map((file) => {
       //console.log(file.photoPath);
       return (
-        <div key={ file.apiPath }>
-          <Link title={file.title} to={ file.uriPath }>
-            <img src={file.photoPath + "?size=300x300&crop"} alt={file.name} loading="lazy" />
-              { file.type === 'video' && <VideoIcon /> }
+        <li key={file.apiPath}>
+          <Link title={file.title} to={file.uriPath}>
+            <img
+              src={file.photoPath + '?size=300x300&crop'}
+              alt={file.name}
+              loading="lazy"
+            />
+            {file.type === 'video' && <VideoIcon />}
           </Link>
           <p>{file.name}</p>
-        </div>
+        </li>
       )
-    });
-    fileList = (
-      <div className="FileList">
-        { fileListItems }
-      </div>
-    );
+    })
+    fileList = <ul className="FileList">{fileListItems}</ul>
   }
 
-  return fileList;
+  return fileList
 }
